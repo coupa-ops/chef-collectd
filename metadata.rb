c319c016b4c5c224421b4351a77b557c1ebeb295
Alok Kumar <alok.kumar@coupa.com>
2019-07-31 21:06:44 -0700

    Adding new files

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             "collectd"
+maintainer       "Noan Kantrowitz"
+maintainer_email "noah@coderanger.net"
+license          "Apache 2.0"
+description      "Install and configure the collectd monitoring daemon"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "1.1.0"
+supports         "ubuntu"
+supports         "centos"
